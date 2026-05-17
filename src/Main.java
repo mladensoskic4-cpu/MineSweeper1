f808c761fb5e03b0cf83df83f92a52cc5f20bdd6
cchiper048 <alan.adzagic48@gmail.com>
2026-05-17 21:37:29 +0200

    Move intelliJ project to the root

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import com.game.Board;
-import com.game.GameOutcome;
 import com.game.GameSimulator;
-import com.game.Player;
 
 void main() {
     GameSimulator.simulateMatches("./data/matches.csv", 1000);
